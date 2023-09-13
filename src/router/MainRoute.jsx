c7122e20d61c49f48b72e3d587b238e8c8065e21
gk072745 <kumawatgovind222@gmail.com>
2023-09-13 08:33:26 +0530

    setup routing

---
+++ src/router/MainRoute.jsx
@@ -0,0 +1,20 @@
+import { Route, Routes } from "react-router";
+import Home from "../Pages/Home";
+import { PrivateRoute } from "./PrivateRouter";
+import Courses from "../Pages/Courses";
+import Signup from "../Pages/Signup";
+import Login from "../Pages/Login";
+
+
+
+const MainRoute = () => {
+    return (
+        <Routes>
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/" element={<PrivateRoute><Home /></PrivateRoute>} />
+            <Route path="/courses" element={<PrivateRoute><Courses /></PrivateRoute>} />
+        </Routes>
+    );
+};
+export default MainRoute;
